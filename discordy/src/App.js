ef63875f7dea4d28ae24dc41be597c2ac1b59d43
TiokengSamuel <samueledward40@gmail.com>
2020-12-26 12:53:39 +0100

    sidebar

--- discordy/src/App.js
+++ discordy/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { Counter } from './features/counter/Counter';
+import { Counter } from './features/Counter';
 import './App.css';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
     <div className="app">
       <h1>Build a Discord Clone</h1>
+
+      {/* Sidebar */}
+      <Sidebar/>
     </div>
   );
 }
