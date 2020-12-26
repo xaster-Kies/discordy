cda281f13963b0de410ca397e5e6e1e6fbcd5fee
TiokengSamuel <samueledward40@gmail.com>
2020-12-26 18:50:39 +0100

    hashtags

---
+++ discordy/src/SidebarChannel.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './SidebarChannel.css'
+
+function SidebarChannel({id, channel}) {
+    return (
+        <div className="sideBarChannel">
+            <h4><span className="sideBarChannel__hash">#</span>JavaScript</h4>
+        </div>
+    )
+}
+
+export default SidebarChannel
