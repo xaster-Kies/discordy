767bf6f1dc57b93edb3aba8005cf296fe457a85a
TiokengSamuel <samueledward40@gmail.com>
2020-12-26 22:33:14 +0100

    Icons

--- discordy/src/ChatHeader.js
+++ discordy/src/ChatHeader.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import './ChatHeader.css'
+import NotificationsIcon from "@material-ui/icons/Notifications"
+import EditLocationRoundedIcon from "@material-ui/icons/EditLocationRounded"
+import PeopleAltRoundedIcon from "@material-ui/icons/PeopleAlt"
+
 
 function ChatHeader() {
     return (
         <div className="chatHeader">
             <div className="chatHeader__left">
-
+                <span className="chatHeader__hash">#</span>
+                    Test Channel Name
             </div>
             <div className="chatHeader__right">
-                
+               <NotificationsIcon/>
+               <EditLocationRoundedIcon/>
+               <PeopleAltRoundedIcon/>
             </div>
         </div>
     )
