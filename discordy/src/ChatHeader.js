e128a58236739904497dbd1ef8ca181550749593
TiokengSamuel <samueledward40@gmail.com>
2020-12-26 22:11:01 +0100

    chat header

---
+++ discordy/src/ChatHeader.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './ChatHeader.css'
+
+function ChatHeader() {
+    return (
+        <div className="chatHeader">
+            
+        </div>
+    )
+}
+
+export default ChatHeader
