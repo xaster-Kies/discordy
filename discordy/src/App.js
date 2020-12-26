0f9bc2be68c6ba283b4d40e1d4883d3e88abf69f
TiokengSamuel <samueledward40@gmail.com>
2020-12-26 22:04:06 +0100

    chat screen

--- discordy/src/App.js
+++ discordy/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Counter } from './features/Counter';
 import './App.css';
 import Sidebar from './Sidebar';
+import Chat from './Chat';
 
 function App() {
   return (
@@ -10,6 +11,8 @@ function App() {
 
       {/* Sidebar */}
       <Sidebar/>
+
+      <Chat/>
     </div>
   );
 }
