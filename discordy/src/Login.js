553ff190276051d17b9a67d7e2d86516490b1907
TiokengSamuel <samueledward40@gmail.com>
2020-12-27 08:29:48 +0100

    button

--- discordy/src/Login.js
+++ discordy/src/Login.js
@@ -1,13 +1,19 @@
+import { Button } from '@material-ui/core'
 import React from 'react'
 import './Login.css'
 
 function Login() {
+    const signIn = () => {
+        //do Clever google Login
+    }
     return (
         <div className="login">
             
             <div className="login__logo">
                 <img src="https://1000logos.net/wp-content/uploads/2020/10/Discord-logo-500x333.png" alt=""/>
             </div>
+
+            <Button onClick={signIn}>Sign In</Button>
         </div>
     )
 }
