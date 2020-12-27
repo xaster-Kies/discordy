1f4f461a2d61fd9e7fbd71909229ae961fb2e1f0
TiokengSamuel <samueledward40@gmail.com>
2020-12-27 08:52:37 +0100

    app

--- discordy/src/App.js
+++ discordy/src/App.js
@@ -5,9 +5,21 @@ import Sidebar from './Sidebar';
 import Chat from './Chat';
 import { selectUser} from './features/userSlice';
 import Login from './Login';
+import { auth } from './firebase';
 
 function App() {
+  const dispatch = useDispatch();
   const user = useSelector(selectUser);
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        //user logged in
+      } else {
+        //user is logged out
+      }
+    })
+  })
   return (
     <div className="app">
     {user ? (
