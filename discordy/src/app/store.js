26d35db4bd50635820dc692633f57b1d9dc7f547
TiokengSamuel <samueledward40@gmail.com>
2020-12-27 05:51:04 +0100

    redux data layer

--- discordy/src/app/store.js
+++ discordy/src/app/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counterSlice';
+import useReducer from '../features/counterSlice';
 
 export default configureStore({
   reducer: {
-    counter: counterReducer,
+    user: userReducer,
   },
 });
