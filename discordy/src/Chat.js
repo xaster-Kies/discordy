bb10da660cf90e967c0286a9f58b215c2f7594be
TiokengSamuel <samueledward40@gmail.com>
2020-12-27 02:13:53 +0100

    add circle

--- discordy/src/Chat.js
+++ discordy/src/Chat.js
@@ -1,11 +1,24 @@
 import React from 'react'
 import './Chat.css'
 import ChatHeader from './ChatHeader'
+import AddCircleIcon from '@material-ui/icons/AddCircle'
 
 function Chat() {
     return (
         <div className="chat">
             <ChatHeader/>
+
+            <div className="chat__messages">
+
+            </div>
+
+            <div className="chat__input">
+                <AddCircleIcon fontSize="large"/>
+                <form>
+                    <input type="text" placeholder={`Message #TESTIMONIAL`}/>
+                    <button></button>
+                </form>
+            </div>
         </div>
     )
 }
