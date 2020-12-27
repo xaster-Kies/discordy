88b417f28cf9f16224aeadbe1ff09cae5ab4c820
TiokengSamuel <samueledward40@gmail.com>
2020-12-27 06:50:07 +0100

    login

--- discordy/src/Login.js
+++ discordy/src/Login.js
@@ -5,6 +5,9 @@ function Login() {
     return (
         <div className="login">
             
+            <div className="login__logo">
+                <img src="https://1000logos.net/wp-content/uploads/2020/10/Discord-logo-500x333.png" alt=""/>
+            </div>
         </div>
     )
 }
