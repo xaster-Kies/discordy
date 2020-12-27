9bd4fdab1250b80c008ee65cc427e403d604b943
TiokengSamuel <samueledward40@gmail.com>
2020-12-27 05:44:37 +0100

    firebase

---
+++ discordy/src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAnM2CTCq5DSABcQbD93gkSVYTDdGZImeM",
+    authDomain: "discordy-79262.firebaseapp.com",
+    projectId: "discordy-79262",
+    storageBucket: "discordy-79262.appspot.com",
+    messagingSenderId: "232230508235",
+    appId: "1:232230508235:web:b1860dd2545f47b0aaed4e",
+    measurementId: "G-ESKYHTRWGL"
+  };
+  
