1fe4a320f776fc1e61ce3d9405e55c4e1f3e9132
TiokengSamuel <samueledward40@gmail.com>
2020-12-27 06:22:03 +0100

    app

--- discordy/src/App.js
+++ discordy/src/App.js
@@ -3,16 +3,21 @@ import { Counter } from './features/Counter';
 import './App.css';
 import Sidebar from './Sidebar';
 import Chat from './Chat';
+import { selectUser} from './features/userSlice';
 
 function App() {
+  const user = useSelector(selectUser);
   return (
     <div className="app">
-    
-
-      {/* Sidebar */}
+    {user ? (
+      <>
       <Sidebar/>
-
       <Chat/>
+      </>
+    ): (
+      <h2>You need to Login</h2>
+    )}
+      
     </div>
   );
 }
